3b235a08101de0bea62bed430f8c7ea98184667f
Partha Murmu <partha@artoo.in>
2015-06-16 15:26:53 +0530

    freshdesk working, few changes left

--- extension/content_script.js
+++ extension/content_script.js
@@ -6,17 +6,19 @@ if(url.substring(0,45)=='https://artoo.freshdesk.com/helpdesk/tickets/'){
 	   username = response.data;
 	   console.log(username);
 	   chrome.runtime.sendMessage({method: "getLocalStorage", key: "mobile"}, function(response) {
-	   mobile = response.data
-	   							var iconPhone = window.document.getElementsByClassName("product-call");
+	   mobile = response.data;
+	   console.log(typeof(mobile) + " : " + mobile)
+	   				var iconPhone = window.document.getElementsByClassName("product-call");
 					
 					es = window.document.getElementsByClassName("can-make-calls hover-contact-box");
 					agentMobile= es[0].getAttribute('data-phone-number');
+					console.log(typeof(agentMobile) + " : " + agentMobile);
 					
 					
-				    callUser = function(){alert("hello kon he?");
+				    callUser = function(){alert("Want to make a Call ?");
 						 				$.post("https://artoo:7565737dd827e04b6cbc3fdf4418cdb167435206@twilix.exotel.in/v1/Accounts/artoo/Calls/connect",
         										{From : mobile,
-						    					To : "08792401743",
+						    					To : agentMobile,
 						    					CallerId : "08039591033",
 						    					CallType : "trans"},
         								function(data, textStatus)
