b7ee05cb0a00b0de0b2d096df7ca5e4432ee8a1d
Sameer Segal <sameer@artoo.in>
2015-09-11 19:08:04 +0530

    added extention built with Ember & Material lite

---
+++ FooExtension/Brocfile.js
@@ -0,0 +1,64 @@
+/* global require, module */
+
+var vulcanize   = require('broccoli-vulcanize');  
+var pickFiles   = require('broccoli-static-compiler');  
+var mergeTrees  = require('broccoli-merge-trees');  
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+var app = new EmberApp();
+
+// Use `app.import` to add additional libraries to the generated
+// output files.
+//
+// If you need to use different assets in different
+// environments, specify an object as the first parameter. That
+// object's keys should be the environment name and the values
+// should be the asset to use in that environment.
+//
+// If the library that you are including contains AMD or ES6
+// modules that you would like to import into your application
+// please specify an object with the list of modules as keys
+// along with the exports of each module as its value.
+
+// var vulcanize = require('broccoli-vulcanize');
+// var inputTree = 'public';
+// var options = {
+//   input: 'elements.html',
+//   output: 'assets/vulcanized.html',
+//   excludes: [/^data:/, /^http[s]?:/, /^\//],
+//   abspath: '/webroot/',
+//   stripExcludes: false,
+//   stripComments: false,
+//   inlineScripts: false,
+//   inlineCss: false,
+//   implicitStrip: false
+// };
+
+// module.exports = vulcanize(app.toTree([]), options);
+
+var polymerVulcanize = vulcanize('app', {  
+  input: 'elements.html',
+  output: 'assets/vulcanized.html',
+  csp: true,
+  inline: true,
+  strip: false,
+  excludes: {
+    imports: ["(^data:)|(^http[s]?:)|(^\/)"],
+    scripts: ["(^data:)|(^http[s]?:)|(^\/)"],
+    styles: ["(^data:)|(^http[s]?:)|(^\/)"]
+  }
+});
+
+var polymer = pickFiles('bower_components/', {  
+  srcDir: '',
+  files: [
+    'webcomponentsjs/webcomponents.js',
+    'polymer/polymer.html'
+//  'polymer/polymer.js'
+  ],
+  destDir: '/assets'
+});
+
+module.exports = mergeTrees([polymerVulcanize, polymer, app.toTree()]);
+
+// module.exports = app.toTree();
